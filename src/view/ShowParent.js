19a50573e5a7e7a2f39dcaa9a998437ea4b28b90
Teerayuth Artidakorn <bomzakup_123@hotmail.com>
2020-02-28 23:52:03 +0700

    Complete Show Parent

--- src/view/ShowParent.js
+++ src/view/ShowParent.js
@@ -1,10 +1,56 @@
 import React,{Component} from 'react';
+import Firebase from '../Firebase';
+import { Icon } from 'semantic-ui-react'
 
 class ShowParent extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state={
+            name: '',
+            imageURL: '',
+            tel: '',
+            line_id: ''
+        }
+    }
+
+    componentDidMount = () =>{
+        Firebase.firestore().collection('parent')
+        .doc(this.props.match.params.id).get()
+        .then(doc => {
+            this.setState({
+                name: doc.data().name,
+                imageURL : doc.data().imageURL,
+                tel: doc.data().tel,
+                line_id: doc.data().line_id
+
+            });
+        })
+    }
+
+
+
     render(){
         return(
             <div>
-                <h1>Show Parent</h1>
+                <br/>
+                <div className="ui items">
+                    <div class="item">
+                    <div class="ui small image">
+                        <a class="ui black ribbon label">
+                            <i aria-hidden="true" class="user icon"></i>
+                            {this.state.name}
+                        </a>
+                        <img src={this.state.imageURL} size />
+                    </div>
+                    <div class="content">
+                        <div class="header">{this.state.name}</div>
+                        <div class="meta"><Icon link name='call' /> {this.state.tel}</div>
+                        <div class="description"><Icon link name='linechat' /> : {this.state.line_id}</div>
+                        <div class="extra">Extra</div>
+                    </div>
+                </div>
+            </div>
             </div>
         )
     }
